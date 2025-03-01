4435d26ad99528402c000d74202d0a8dd6370cf9
Azeezat Oyekale <azeezatoyekale@gmail.com>
2025-03-01 21:56:27 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,6 @@ variable "aws_access_key" {
 }
 
 variable "aws_secret_key" {
-  description = "YYhTAtC2/oa0TmmM7WH/ADY5MwNGg1U5jvpcr1Y1"
+  description = "hdgsavsagdgxvhsav"
   type        = string
 }
